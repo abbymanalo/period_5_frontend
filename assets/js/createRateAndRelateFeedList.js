320d595d852f21effed337a502d452878f9ba46d
shriyasshah <shriya.s.shah@gmail.com>
2024-11-19 21:50:26 -0800

    frontend javascript

--- assets/js/createRateAndRelateFeedList.js
+++ assets/js/createRateAndRelateFeedList.js
@@ -22,3 +22,49 @@ async function generateImage(int) {
         console.error('Error fetching data:', error);
     }
 }
+
+// Function to create an image post with caption and user/date info
+export async function createImagePost(postObj) {
+    // Destructure the object to extract necessary details
+    const imageData = await generateImage(postObj.id)
+    const imageUrl = imageData.postImg
+    console.log(imageUrl)
+    const caption = postObj.title;
+    const user = postObj.user_name;
+    const datePosted = "10/10/10";
+
+    // Create a container for the post
+    const postContainer = document.createElement("div");
+    postContainer.classList.add("postFeed");
+
+    // Create the image element
+    const imageDiv = document.createElement("div");
+    imageDiv.classList.add("imageContainer");
+    const imageElement = document.createElement("img");
+    imageElement.src = `data:image/jpeg;base64,${imageUrl}`;
+    imageDiv.appendChild(imageElement);
+
+    // Create the caption container
+    const captionDiv = document.createElement("div");
+    captionDiv.classList.add("textContainer");
+    const captionElement = document.createElement("h3");
+    captionElement.innerHTML = caption;
+    captionDiv.append(captionElement)
+    
+    // Create the user and date info element
+    const infoDiv = document.createElement("div");
+    infoDiv.classList.add("textInfo");
+    const datePostedElement = document.createElement("p");
+    datePostedElement.innerHTML = "Date Posted: " + datePosted;
+    infoDiv.append(datePostedElement);
+    const userPostedElement = document.createElement("p");
+    userPostedElement.innerHTML = "Posted by: " + user
+    infoDiv.append(userPostedElement);
+    
+    // Append everything to the post container
+    postContainer.appendChild(imageDiv);
+    postContainer.appendChild(captionDiv);
+    postContainer.appendChild(infoDiv);
+    
+    return postContainer
+}
